cea63f1886d691dab3be6ad33bcee45d08b6421b
kimjunghun1 <kimjungheun0@gmail.com>
2025-11-14 13:07:24 +0900

    modify fibonacci function

--- src/main/java/kr/ac/hansung/Fibonacci.java
+++ src/main/java/kr/ac/hansung/Fibonacci.java
@@ -18,6 +18,7 @@ public class Fibonacci {
             int fib= fibonacci(i);
             System.out.print("fibonacci(" + i + ")=" + fib);
             System.out.println();
+            int j = 0;
         }
     }
 
